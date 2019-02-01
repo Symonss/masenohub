dd09e0d69b40b071466cc09a3a836b5a19a91c94
coderjaymoh <lelerukjaymoh@gmail.com>
2019-02-01 17:45:00 +0400

    navbar restructure

--- navigation.php
+++ navigation.php
@@ -2,18 +2,19 @@
           <ul class="nav navbar-nav navbar-right">
             <li ><a class="page-scroll" href="#home">Home</a></li>
             <li> <a class="page-scroll" href="#about">about us</a></li>
-            <li> <a class="page-scroll" href="#services">what we do</a></li>
-            <li> <a class="page-scroll" href="#product">product</a></li>
+            <li> <a class="page-scroll" href="#services">projects</a></li>
+            <li> <a class="page-scroll" href="#product">events</a></li>
+            <li> <a class="page-scroll" href="gallery.php">resources</a></li>
             <li> <a class="page-scroll" href="#team">team</a></li>
-            <li> <a class="page-scroll" href="gallery.php">Gallery</a></li>
             <li class="dropdown"> <a class="page-scroll drop dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" href="#blog">blog</a>
               <ul class="dropdown-menu" role="menu">
                 <!-- <li><a href="blog-full-width.php">full-width Blog</a></li> -->
                 <li><a href="blog-sidebar.php">sidebar blog</a></li>
               </ul>
             </li>
-        
+
           <li><a class="page-scroll" href="#contact">Contact</a></li>
+          <li><a class="page-scroll" href="blog-sidebar.php">forum</a></li>
           <li><a class="page-scroll" href="#" data-toggle="modal" data-target="#LoginModal" >Login</a></li>
            <!-- Search Block -->
           <li>
@@ -25,7 +26,7 @@
                   <button class="btn-u" type="button">Go</button>
                 </span>
               </div>
-            </div>    
+            </div>
           </li>
           <!-- End Search Block -->
         </ul>
